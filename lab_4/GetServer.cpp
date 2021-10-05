48665d9f2d29752459849f1c9eb65ea8812d2d83
antony <tehanovanton@gmail.com>
2021-10-05 21:07:10 +0300

    server and client saying hello to each other

--- lab_4/GetServer.cpp
+++ lab_4/GetServer.cpp
@@ -11,8 +11,8 @@ bool GetServer(char* call, short port, sockaddr* from, int* lenFrom)
 	//bind
 	SOCKADDR_IN cSAddrIn;
 	cSAddrIn.sin_family = AF_INET;
-	cSAddrIn.sin_addr.S_un.S_addr = inet_addr(WIRELESS_IPV4);
-	cSAddrIn.sin_port = htons(PORT);
+	cSAddrIn.sin_addr.S_un.S_addr = inet_addr(SERVER_IPV4);
+	cSAddrIn.sin_port = htons(SERVER_PORT);
 
 	if (bind(cS, (SOCKADDR*)&cSAddrIn, sizeof(SOCKADDR_IN)) != 0)
 		throw SetErrorMsgText("Bind: ", WSAGetLastError());
@@ -20,39 +20,29 @@ bool GetServer(char* call, short port, sockaddr* from, int* lenFrom)
 	//setsockopt
 	int optval = 1;
 	if (setsockopt(cS, SOL_SOCKET, SO_BROADCAST, (char*)&optval, sizeof(int)) == SOCKET_ERROR)
-		throwError(SOCKOPT_MSG_TEXT);
+		throwError("Setsockopt: ");
 
 	//sendto
 	SOCKADDR_IN all;
 	all.sin_family = AF_INET;
 	all.sin_addr.S_un.S_addr = INADDR_BROADCAST;
 	all.sin_port = htons(port);
-					 
+
 	string call_msg = call;
 	if (sendto(cS, call, call_msg.size() + 1, 0, (SOCKADDR*)&all, sizeof(all)) == SOCKET_ERROR)
 		throwError(SENDTO_MSG_TEXT);
 
-	cout << "Me: "
-		<< inet_ntoa(cSAddrIn.sin_addr) << ":" << htons(cSAddrIn.sin_port) << "\n\n";
-
-
-	cout << "Sent message: " << call<< "; "
+	cout << "Sent message: " << call << "; "
 		<< inet_ntoa(all.sin_addr) << ":" << htons(all.sin_port) << "\n\n";
 
 	//recvfrom	 
 	SOCKADDR_IN sSAddrIn;
 	int sizeSSAddrIn = sizeof(sSAddrIn);
 	char buf[10];
+	if (recvfrom(cS, buf, sizeof(buf), 0, (SOCKADDR*)&sSAddrIn, &sizeSSAddrIn) == SOCKET_ERROR)
+		throwError(RECVFROM_MSG_TEXT);
+
 	bool res_recvfrom;
-	bool isCS = true;
-	while (res_recvfrom = strcmp(buf, call) != 0 || isCS)
-	{
-		if (recvfrom(cS, buf, sizeof(buf), 0, (SOCKADDR*)&sSAddrIn, &sizeSSAddrIn) == SOCKET_ERROR)
-			throwError(RECVFROM_MSG_TEXT);
-
-		isCS = strcmp(inet_ntoa(sSAddrIn.sin_addr), WIRELESS_IPV4) == 0;
-	}
-
 	if (res_recvfrom = strcmp(buf, call) == 0)
 	{
 		*from = *((SOCKADDR*)&sSAddrIn);
